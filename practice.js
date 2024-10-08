f9efc5ba373759b3ba63a18ece63ef64c2228513
RattieGirl <m_grigoryeva@outlook.com>
2024-10-08 08:28:51 -0700

    Added Python questions, new version of quiz without global variables

---
+++ practice.js
@@ -0,0 +1,37 @@
+let a = Number(prompt("munber"));
+
+function first(){
+
+  if (a === 0){
+    b = a + 10
+  }
+  else{
+    b = a + 20
+  }
+  console.log(b)
+}
+
+
+first()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (i=0; i < )
---
+++ practice.py
@@ -0,0 +1,21 @@
+a = int(input())
+
+def first():
+  if (a == 0):
+    b = a + 10
+  else:
+    b = a + 20
+  print(b)
+
+first()
+
+
+
+
+
+# n = input()
+# i = n
+# result = 0
+
+# while True
+
